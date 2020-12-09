3a4ae3f26c02de711d892ae2b5268df775710a9f
sangharsh <sangharshbyss@gmail.com>
2020-12-09 06:55:53 +0530

    unknown changes

--- data_summary.py
+++ data_summary.py
@@ -1,7 +1,7 @@
 import os
 
 
-def poa_district_summary(poa_summary_list, district, date):
+def poa_district_summary(poa_summary_list, district, police_station,date):
     """convert list of cases in csv file
     1. column headings are set with variables
     2. list generated from main function, to be used as parameter
@@ -9,7 +9,13 @@ def poa_district_summary(poa_summary_list, district, date):
     4. date is same as command line argument from main.
     """
     directory = '/home/sangharsh/Documents/data/FIR_Data/summary'
-    with open(os.path.join(directory, f'{district}{date}.txt'), 'w') as f:
+    with open(os.path.join(directory, f'{district}_{police_station}{date}.txt'), 'w') as f:
         f.write(f"{poa_summary_list}")
+        f.close()
+
+def non_poa_district_summary(non_poa_list, district, police_station, date):
+    directory = '/home/sangharsh/Documents/data/FIR_Data/summary'
+    with open(os.path.join(directory, f'alt_{district}_{police_station}{date}.txt'), 'w') as f:
+        f.write(f"{non_poa_list}")
         f.close()
 
