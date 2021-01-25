a416119b0a5e4c755f9feea6c680563affcf78a9
sangharsh <sangharshbyss@gmail.com>
2021-01-25 13:07:32 +0530

    minor changes

--- sendingMail.py
+++ sendingMail.py
@@ -5,13 +5,13 @@ the_text = """<p style="text-align: center;"><strong>D</strong><span>ata</span><
 <p style="text-align: center;"><span>Secretariat,&nbsp;</span><strong>DLac</strong><strong>,<span>&nbsp;</span></strong><span>℅, Manuski, Deccan College Road, Yerwada, Pune 411006.&nbsp;</span></p>
 <p style="text-align: center;"><a href="http://www.manuski.in/" target="_blank" rel="noopener noreferrer"><span>www.manuski.in</span></a><span>,&nbsp;</span><a href="mailto:info@manuski.in" rel="noreferrer"><span>info@manuski.in</span></a><span>,&nbsp; संपर्क ८९५६५७८७६३.&nbsp;</span></p>
 <hr />
-<p><span>Ref: PoA/FIR/2021/01/03&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;</span><span>१९/०१/२०२१</span></p>
+<p><span>Ref: PoA/FIR/2021/01/06&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;</span><span>२५/०१/२०२१</span></p>
 <p><span>जय भीम,</span></p>
-<p><span>दिनांक १ जानेवारी ते दिनांक १२ जानेवारी २०२१ या काळात महाराष्ट्रात नोंद झालेल्या गुन्ह्यांचा सारांश व प्रथम खबरी अहवाल (FIR) च्या प्रति इमेल द्वारे बऱ्याच जनांपर्यंत पोचवण्याचा प्रयत्न करण्यात आला. आता १३ जानेवारी ते १५ जानेवारी २०२१ पर्यंत महाराष्ट्रात अनुसूचित जाती आणि अनुसूचित जमाती अत्याचार प्रतिबंधक अधिनियम १९८९ अनुसार नोंद झालेल्या गुन्ह्यांचा सारांश खाली देण्यात येत आहे. सदर गुन्ह्यांच्या एफ.आय. आर. च्या प्रती सोबत जोडल्या आहेत. जातीय अत्याचाराने पीडित व्यक्तिंना योग्य तो कायदेशीर सल्ला&nbsp; व मानसिक पाठबळ मिळण्यासाठी यातून मदत व्हावी हि अपेक्षा. संविधानिक उद्देशांना गतिमान करण्याच्या हेतूने या माहितीचा कुठलीही संस्था किंवा व्यक्ती मुक्त वापर करून शकते. सदर उपयोग करताना डेटा लॅब (डी-लॅक) ने केलेल्या या प्रयत्नांचा उल्लेख करून योग्य ते श्रेयनिर्देश दिल्यास या प्रकल्पाला पाठबळ मिळेल याची कृपया नोंद घ्यावी. सदर माहितीत&nbsp; कुठलीही विसंगती आढळल्यास कृपया निदर्शनात आणून सहकार्य करावे.&nbsp;</span></p>
+<p><span>ददिनांक १ जानेवारी ते दिनांक १९ जानेवारी २०२१ या काळात महाराष्ट्रात नोंद झालेल्या गुन्ह्यांचा सारांश व प्रथम खबरी अहवाल (FIR) च्या प्रति इमेल द्वारे बऱ्याच जनांपर्यंत पोचवण्याचा प्रयत्न करण्यात आला. आता २० जानेवारी ते २१ जानेवारी २०२१ पर्यंत महाराष्ट्रात अनुसूचित जाती आणि अनुसूचित जमाती अत्याचार प्रतिबंधक अधिनियम १९८९ अनुसार नोंद झालेल्या गुन्ह्यांचा सारांश खाली देण्यात येत आहे. सदर गुन्ह्यांच्या एफ.आय. आर. च्या प्रती सोबत जोडल्या आहेत. जातीय अत्याचाराने पीडित व्यक्तिंना योग्य तो कायदेशीर सल्ला&nbsp; व मानसिक पाठबळ मिळण्यासाठी यातून मदत व्हावी हि अपेक्षा. संविधानिक उद्देशांना गतिमान करण्याच्या हेतूने या माहितीचा कुठलीही संस्था किंवा व्यक्ती मुक्त वापर करून शकते. सदर उपयोग करताना डेटा लॅब (डी-लॅक) ने केलेल्या या प्रयत्नांचा उल्लेख करून योग्य ते श्रेयनिर्देश दिल्यास या प्रकल्पाला पाठबळ मिळेल याची कृपया नोंद घ्यावी. सदर माहितीत&nbsp; कुठलीही विसंगती आढळल्यास कृपया निदर्शनात आणून सहकार्य करावे.&nbsp;</span></p>
 <p>&nbsp;</p>
 <p><strong>विशेष</strong><span>:&nbsp; सदर माहिती मध्ये केवळ नोंद झालेल्या व पोलिसांमार्फत संकेत स्थळावर उपलब्ध करण्यात आलेल्या गुन्ह्यांचीची आकडेवारी दिली आहे. काही विशेष स्वरूपाच्या गुन्ह्यांची माहिती (उदाहरणार्थ लैंगिक अत्याचाराची प्रकरणे) अशा पद्धतीने संकेतस्थळावर न देण्याच्या विशेष सूचना आहेत. त्यानुसार हे गुन्हे संकेतस्थळावर उपलब्ध नाहीत व ते आकडेवारीत दिसत नाहीत. तसेच काही पोलिस स्टेशन त्यांच्याकडील&nbsp; नोंद गुन्ह्याच्या प्रति संकेत स्थळावर उपलब्ध करून देत नाहीत त्यामुळे त्यांचीही नोंद दिलेल्या आकडेवारी मध्ये नाही.&nbsp;</span></p>
 <p>&nbsp;</p>
-<p><strong>दिनांक १३ जानेवारी ते १५ जानेवारी २०२१ याकाळात महाराष्ट्रात अनुसूचित जाती आणि अनुसूचित जमाती अत्याचार प्रतिबंधक अधिनियम १९८९ नोंद झालेल्या तक्रारींचा सारांश:</strong></p>
+<p><strong>ददिनांक २० जानेवारी व २१ जानेवारी २०२१ याकाळात महाराष्ट्रात अनुसूचित जाती आणि अनुसूचित जमाती अत्याचार प्रतिबंधक अधिनियम १९८९ नोंद झालेल्या तक्रारींचा सारांश:</strong></p>
 <p>&nbsp;</p>
 <table style="background-color: #000000;" cellspacing="2">
 <tbody>
@@ -159,7 +159,7 @@ the_text = """<p style="text-align: center;"><strong>D</strong><span>ata</span><
 </table>
 <p>&nbsp;</p>
 <p><span>&nbsp;</span></p>
-<p><span>वरील सारणी वरून हे स्पष्ट होते कि, दिनांक १३ जानेवारी ते दिनांक १५ जानेवारी २०२१ या काळात म्हणजेच ३ दिवसांमध्ये, अनुसूचित जाती व अनुसूचित जमाती अत्याचार प्रतिबंधक कायदा १९८९ अनुसार कमीत कमी ६ गुन्हे नोंद झालेले आहेत. यापैकी सोलापूर ग्रामीण या विभागात ३ गुन्ह्यांची नोंद झाल्याचे दिसून येते. तसेच ठाणे शहर, नवी मुंबई व औरंगाबाद ग्रामीण या ठिकाणी या जिल्ह्यांमध्ये प्रत्येकी एक गुन्ह्याची नोंद झाल्याचे दिसून येते.&nbsp;&nbsp;</span></p>
+<p><span>वरील सारणी वरून हे स्पष्ट होते कि, दिनांक २० जानेवारी व दिनांक २१ जानेवारी २०२१ या काळात म्हणजेच २ दिवसांमध्ये, अनुसूचित जाती व अनुसूचित जमाती अत्याचार प्रतिबंधक कायदा १९८९ अनुसार कमीत कमी ६ गुन्हे नोंद झालेले आहेत. यापैकी सोलापूर ग्रामीण या विभागात २ गुन्ह्यांची नोंद झाल्याचे दिसून येते. तसेच कोल्हापूर, नांदेड, नाशिक, सांगली या जिल्ह्यांमध्ये प्रत्येकी एक गुन्ह्याची नोंद झाल्याचे दिसून येते.&nbsp;&nbsp;</span></p>
 <p>&nbsp;</p>
 <p><span>डेटा&nbsp; टीम&nbsp;</span></p>
 <p><span>संपर्क:&nbsp;</span></p>
@@ -208,8 +208,9 @@ for name in receivers:
              subject='(Marathi)Update: Complaints registered '
                      'under PoA on 18-19 January 2021',
              contents=the_text,
-             attachments=['/home/sangharsh/Documents/PoA/data/FIR/January/copies/18012021 _ 19012021/rptFIR_Publish_Citizen.pdf',
+             attachments=['/home/sangharsh/Documents/PoA/data/FIR/January/copies/20012021 _ 21012021/rptFIR_Publish_Citizen.pdf',
                           '/home/sangharsh/Documents/PoA/data/FIR/January/copies/18012021 _ 19012021/rptFIR_Publish_Citizen(1).pdf',
                           '/home/sangharsh/Documents/PoA/data/FIR/January/copies/18012021 _ 19012021/rptFIR_Publish_Citizen(2).pdf',
                           '/home/sangharsh/Documents/PoA/data/FIR/January/copies/18012021 _ 19012021/rptFIR_Publish_Citizen(3).pdf'])
+
 print(f'Email succesfully sent to {len(receivers) } participants')
