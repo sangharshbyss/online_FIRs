ea4c24f3bc043a364ae56de7a3d5c4fc21fe5472
sangharsh <sangharshbyss@gmail.com>
2021-02-02 12:38:39 +0530

    html content for 28-29 changed

--- sendingMail.py
+++ sendingMail.py
@@ -1,18 +1,17 @@
 # importing the Yagmail library
 import yagmail
 
-the_text = """<p style="text-align: center;"><strong>D</strong><span>ata</span><strong><span>&nbsp;</span>L</strong><span>ab</span><span>&nbsp;</span><span>for</span><span>&nbsp;</span><strong>A</strong><span>nnihilation</span><span>&nbsp;</span><span>of</span><span>&nbsp;</span><strong>C</strong><span>aste</span><strong>&nbsp;</strong></p>
-<p style="text-align: center;"><span>Secretariat,&nbsp;</span><strong>DLac</strong><strong>,<span>&nbsp;</span></strong><span>℅, Manuski, Deccan College Road, Yerwada, Pune 411006.&nbsp;</span></p>
+the_text = """<p style="text-align: center;"><strong>D</strong><span>ata</span><strong><span>&nbsp;</span>L</strong><span>ab</span><span>&nbsp;</span><span>for</span><span>&nbsp;</span><strong>A</strong><span>nnihilation</span><span>&nbsp;</span><span>of</span><span>&nbsp;</span><strong>C</strong><span>aste</span><strong></strong></p>
+<p style="text-align: center;"><span>Secretariat,&nbsp;</span><strong>DLac</strong><strong>,<span>&nbsp></strong><span>℅, Manuski, Deccan College Road, Yerwada, Pune 411006.&nbsp;</span></p>
 <p style="text-align: center;"><a href="http://www.manuski.in/" target="_blank" rel="noopener noreferrer"><span>www.manuski.in</span></a><span>,&nbsp;</span><a href="mailto:info@manuski.in" rel="noreferrer"><span>info@manuski.in</span></a><span>,&nbsp; संपर्क ८९५६५७८७६३.&nbsp;</span></p>
 <hr />
-<p><span>Ref: PoA/FIR/2021/01/03&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;</span><span>१/०१/२०२१</span></p>
+<p><span>Ref: PoA/FIR/2021/02/02&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;</span><span>०२/०२/२०२१</span></p>
 <p><span>जय भीम,</span></p>
-<p><span>दिनांक १ जानेवारी ते दिनांक २१ जानेवारी २०२१ या काळात महाराष्ट्रात नोंद झालेल्या गुन्ह्यांचा सारांश व प्रथम खबरी अहवाल (FIR) च्या प्रति इमेल द्वारे बऱ्याच जनांपर्यंत पोचवण्याचा प्रयत्न करण्यात आला. आता २२ जानेवारी ते २२ जानेवारी २०२१ पर्यंत महाराष्ट्रात अनुसूचित जाती आणि अनुसूचित जमाती अत्याचार प्रतिबंधक अधिनियम १९८९ अनुसार नोंद झालेल्या गुन्ह्यांचा सारांश खाली देण्यात येत आहे. सदर गुन्ह्यांच्या एफ.आय. आर. च्या प्रती सोबत जोडल्या आहेत. जातीय अत्याचाराने पीडित व्यक्तिंना योग्य तो कायदेशीर सल्ला&nbsp; व मानसिक पाठबळ मिळण्यासाठी यातून मदत व्हावी हि अपेक्षा. संविधानिक उद्देशांना गतिमान करण्याच्या हेतूने या माहितीचा कुठलीही संस्था किंवा व्यक्ती मुक्त वापर करून शकते. सदर उपयोग करताना डेटा लॅब (डी-लॅक) ने केलेल्या या प्रयत्नांचा उल्लेख करून योग्य ते श्रेयनिर्देश दिल्यास या प्रकल्पाला पाठबळ मिळेल याची कृपया नोंद घ्यावी. सदर माहितीत&nbsp; कुठलीही विसंगती आढळल्यास कृपया निदर्शनात आणून सहकार्य करावे.&nbsp;</span></p>
+<p><span>दिनांक १ जानेवारी ते दिनांक २७ जानेवारी २०२१ या काळात महाराष्ट्रात नोंद झालेल्या गुन्ह्यांचा सारांश व प्रथम खबरी अहवाल (FIR) च्या प्रति इमेल द्वारे बऱ्याच जनांपर्यंत पोचवण्याचा प्रयत्न करण्यात आला. आता २८ जानेवारी व २९ जानेवारी २०२१ रोजी महाराष्ट्रात अनुसूचित जाती आणि अनुसूचित जमाती अत्याचार प्रतिबंधक अधिनियम १९८९ अनुसार नोंद झालेल्या गुन्ह्यांचा सारांश खाली देण्यात येत आहे. सदर गुन्ह्यांच्या एफ.आय. आर. च्या प्रती सोबत जोडल्या आहेत. जातीय अत्याचाराने पीडित व्यक्तिंना योग्य तो कायदेशीर सल्ला&nbsp; व मानसिक पाठबळ मिळण्यासाठी यातून मदत व्हावी हि अपेक्षा. संविधानिक उद्देशांना गतिमान करण्याच्या हेतूने या माहितीचा कुठलीही संस्था किंवा व्यक्ती मुक्त वापर करून शकते. सदर उपयोग करताना डेटा लॅब (डी-लॅक) ने केलेल्या या प्रयत्नांचा उल्लेख करून योग्य ते श्रेयनिर्देश दिल्यास या प्रकल्पाला पाठबळ मिळेल याची कृपया नोंद घ्यावी. सदर माहितीत कुठलीही विसंगती आढळल्यास कृपया निदर्शनात आणून सहकार्य करावे.&nbsp;</span></p>
 <p>&nbsp;</p>
-<p><strong>विशेष</strong><span>:&nbsp; सदर माहिती मध्ये केवळ नोंद झालेल्या व पोलिसांमार्फत संकेत स्थळावर उपलब्ध करण्यात आलेल्या गुन्ह्यांचीची आकडेवारी दिली आहे. काही विशेष स्वरूपाच्या गुन्ह्यांची माहिती (उदाहरणार्थ लैंगिक अत्याचाराची प्रकरणे) अशा पद्धतीने संकेतस्थळावर न देण्याच्या विशेष सूचना आहेत. त्यानुसार हे गुन्हे संकेतस्थळावर उपलब्ध नाहीत व ते आकडेवारीत दिसत नाहीत. तसेच काही पोलिस स्टेशन त्यांच्याकडील&nbsp; नोंद गुन्ह्याच्या प्रति संकेत स्थळावर उपलब्ध करून देत नाहीत त्यामुळे त्यांचीही नोंद दिलेल्या आकडेवारी मध्ये नाही.&nbsp;</span></p>
+<p><strong>विशेष</strong><span>:&nbsp; सदर माहिती मध्ये केवळ नोंद झालेल्या व पोलिसांमार्फत संकेत स्थळावर उपलब्ध करण्यात आलेल्या <a href="https://citizen.mahapolice.gov.in/Citizen/MH/PublishedFIRs.aspx/robot.txt">(Maharashtra Police Published FIRs)</a> गुन्ह्यांचीची आकडेवारी दिली आहे. काही विशेष स्वरूपाच्या गुन्ह्यांची माहिती (उदाहरणार्थ लैंगिक अत्याचाराची प्रकरणे) अशा पद्धतीने संकेतस्थळावर न देण्याच्या विशेष सूचना आहेत. त्यानुसार हे गुन्हे संकेतस्थळावर उपलब्ध नाहीत व ते आकडेवारीत दिसत नाहीत. तसेच काही पोलिस स्टेशन त्यांच्याकडील&nbsp; नोंद गुन्ह्याच्या प्रति संकेत स्थळावर उपलब्ध करून देत नाहीत त्यामुळे त्यांचीही नोंद दिलेल्या आकडेवारी मध्ये नाही.&nbsp;</span></p>
 <p>&nbsp;</p>
-<p><strong>दिनांक २२ जानेवारी व २३ जानेवारी २०२१ याकाळात महाराष्ट्रात अनुसूचित जाती आणि अनुसूचित जमाती अत्याचार प्रतिबंधक अधिनियम १९८९ नोंद झालेल्या तक्रारींचा सारांश:</strong></p>
-<p>&nbsp;</p>
+<p><strong>दिनांक २८ जानेवारी व २९ जानेवारी २०२१ रोजी महाराष्ट्रात अनुसूचित जाती आणि अनुसूचित जमाती अत्याचार प्रतिबंधक अधिनियम १९८९ नोंद झालेल्या तक्रारींचा सारांश:</strong></p>
 <table style="background-color: #000000;" cellspacing="2">
 <tbody>
 <tr style="background-color: #bab6b6;">
@@ -32,7 +31,7 @@ the_text = """<p style="text-align: center;"><strong>D</strong><span>ata</span><
 <p><strong>Date</strong></p>
 </td>
 <td>
-<p><strong>Acts_&amp;_Sections</strong></p>
+<p><strong>Acts &amp; Sections</strong></p>
 </td>
 </tr>
 <tr style="background-color: #ffffff;">
@@ -40,19 +39,18 @@ the_text = """<p style="text-align: center;"><strong>D</strong><span>ata</span><
 <p><span>1</span></p>
 </td>
 <td>
-<p><span>AURANGABAD RURAL</span></p>
+<p><span>AHMEDNAGAR</span></p>
 </td>
 <td>
-<p><span>VAIJAPUR</span></p>
+<p><span>SHRIGONDA</span></p>
 </td>
 <td>
-<p><span>21</span></p>
+<p><span>85</span></p>
 </td>
 <td>
-<p><span>13/01/2021&nbsp;</span></p>
+<p><span>28/01/2021&nbsp;</span></p>
 </td>
-<td>
-<p><span>भारतीय दंड संहिता १८६० - ३२३,५०४,५०६ ; अनुसूचीत जाती आणि अनुसूचीत जमाती (अत्&zwj;याचार प्रतिबंधक) अधिनियम, १९८९ - 3(1)(r),3(1)(s) ;</span></p>
+<td>शस्‍त्र अधिनियम, १९५९ - 25,4 ; भारतीय दंड संहिता १८६० - ३०७,३२६,३४,३४१,५०४ ; अनुसूचीत जाती आणि अनुसूचीत जमाती (अत्‍याचार प्रतिबंधक) अधिनियम, १९८९ - 3(1)(r),3(1)(s),3(2)(va) ; </span></p>
 </td>
 </tr>
 <tr style="background-color: #ffffff;">
@@ -60,19 +58,19 @@ the_text = """<p style="text-align: center;"><strong>D</strong><span>ata</span><
 <p><span>2</span></p>
 </td>
 <td>
-<p><span>SOLAPUR RURAL</span></p>
+<p><span>BEED</span></p>
 </td>
 <td>
-<p><span>PANDHARPUR TALUKA</span></p>
+<p><span>AMBAJOGAI RURAL</span></p>
 </td>
 <td>
-<p><span>32</span></p>
+<p><span>20</span></p>
 </td>
 <td>
-<p><span>13/01/2021&nbsp;</span></p>
+<p><span>28/01/2021&nbsp;</span></p>
 </td>
 <td>
-<p><span>भारतीय दंड संहिता १८६० - १४३,१४७,१४८,१४९,३२३,३२६,५०४,५०६ ; अनुसूचीत जाती आणि अनुसूचीत जमाती (अत्&zwj;याचार प्रतिबंधक) अधिनियम, १९८९ - 3(1)(r),3(1)(s) ; महाराष्ट्र पोलीस अधिनियम, १९५१ - 135 ;</span></p>
+<p><span>भारतीय दंड संहिता १८६० - ३४,509 (ख),५०४,५०६,५०७ ; अनुसूचीत जाती आणि अनुसूचीत जमाती (अत्‍याचार प्रतिबंधक) अधिनियम, १९८९ - 3(1)(r),3(1)(s) </span></p>
 </td>
 </tr>
 <tr style="background-color: #ffffff;">
@@ -80,19 +78,19 @@ the_text = """<p style="text-align: center;"><strong>D</strong><span>ata</span><
 <p><span>3</span></p>
 </td>
 <td>
-<p><span>SOLAPUR RURAL</span></p>
+<p><span>BULDHANA</span></p>
 </td>
 <td>
-<p><span>SOLAPUR</span></p>
+<p><span>DHAMANGAONBADHE</span></p>
 </td>
 <td>
-<p><span>44</span></p>
+<p><span>21</span></p>
 </td>
 <td>
-<p><span>13/01/2021</span></p>
+<p><span>28/01/2021</span></p>
 </td>
 <td>
-<p><span>भारतीय दंड संहिता १८६० - १४३,१४७,१४८,३२३,३२६,५०४,५०६ ; अनुसूचीत जाती आणि अनुसूचीत जमाती (अत्&zwj;याचार प्रतिबंधक) अधिनियम, १९८९ - 3(1)(r),3(1)(s),3(2)(va) ;</span></p>
+<p><span>भारतीय दंड संहिता १८६० - २९४,३२४,३४ ; अनुसूचीत जाती आणि अनुसूचीत जमाती (अत्‍याचार प्रतिबंधक) अधिनियम, १९८९ - 3(1)(r),3(1)(s),3(2)(va)</span></p>
 </td>
 </tr>
 <tr style="background-color: #ffffff;">
@@ -100,26 +98,43 @@ the_text = """<p style="text-align: center;"><strong>D</strong><span>ata</span><
 <p><span>4</span></p>
 </td>
 <td>
-<p><span>THANE CITY</span></p>
+<p><span>NAGPUR RURAL</span></p>
 </td>
 <td>
-<p><span>NAUPADA</span></p>
+<p><span>KELWAD</span></p>
 </td>
 <td>
-<p><span>11</span></p>
+<p><span>17</span></p>
 </td>
 <td>
-<p><span>13/01/2021</span></p>
+<p><span>27/01/2021</span></p>
 </td>
 <td>
-<p><span>भारतीय दंड संहिता १८६० - ३२३,३४,४०६,४२०,४६५,४६७,४६८,४७१,५०४,५०६ ; अनुसूचीत जाती आणि अनुसूचीत जमाती (अत्&zwj;याचार प्रतिबंधक) अधिनियम, १९८९ - 3,3(1)(r),3(1)(s) ;</span></p>
+<p><span>भारतीय दंड संहिता १८६० - २९४,३४,५०६ ; अनुसूचीत जाती आणि अनुसूचीत जमाती (अत्‍याचार प्रतिबंधक) अधिनियम, १९८९ - 3(1)(r),3(1)(s) ;</span></p>
 </td>
 </tr>
-</tbody>
+<tr style="background-color: #ffffff;">
+<td>
+<p><span>5</span></p>
+</td>
+<td>
+<p><span>NANDED</span></p>
+</td>
+<td>
+<p><span>MUKHED</span></p>
+</td>
+<td>
+<p><span>30</span></p>
+</td>
+<td>
+<p><span>29/01/2021</span></p>
+</td>
+<td>
+<p><span>भारतीय दंड संहिता १८६० - १४३,१४९,२९४,३२३ ; अनुसूचीत जाती आणि अनुसूचीत जमाती (अत्‍याचार प्रतिबंधक) अधिनियम, १९८९ - 3(1)(r),3(2)</span></p>
+</td>
+</tr>
 </table>
-<p>&nbsp;</p>
-<p><span>&nbsp;</span></p>
-<p><span>वरील सारणी वरून हे स्पष्ट होते कि, दिनांक २२ जानेवारी व दिनांक २२ जानेवारी २०२१ या काळात म्हणजेच २ दिवसांमध्ये, अनुसूचित जाती व अनुसूचित जमाती अत्याचार प्रतिबंधक कायदा १९८९ अनुसार कमीत कमी ६ गुन्हे नोंद झालेले आहेत. यापैकी बीड या विभागात २ गुन्ह्यांची नोंद झाल्याचे दिसून येते. तसेच परभणी व सोलापूर या जिल्ह्यांमध्ये प्रत्येकी एक गुन्ह्याची नोंद झाल्याचे दिसून येते.&nbsp;&nbsp;</span></p>
+<p><span>वरील सारणी वरून हे स्पष्ट होते कि, दिनांक २८ जानेवारी व दिनांक २९ जानेवारी २०२१ या काळात म्हणजेच २ दिवसांमध्ये, अनुसूचित जाती व अनुसूचित जमाती अत्याचार प्रतिबंधक कायदा १९८९ अनुसार कमीत कमी ५ गुन्हे नोंद झालेले आहेत. अहमदनर, बीड, नागपूर ग्रामीण, नांदेड, बुलढाणा या जिल्ह्यांमध्ये प्रत्येकी एक गुन्ह्याची नोंद झाल्याचे दिसून येते.&nbsp;&nbsp;</span></p>
 <p>&nbsp;</p>
 <p><span>डेटा&nbsp; टीम&nbsp;</span></p>
 <p><span>संपर्क:&nbsp;</span></p>
@@ -131,8 +146,9 @@ the_text = """<p style="text-align: center;"><strong>D</strong><span>ata</span><
 # initializing the server connection
 yag = yagmail.SMTP(user='dlac.automated@gmail.com', password='AB@ab@12')
 # receiver list
-receivers = ["sangharshbyss@gmail.com", "kapilshivsharan37@gmail.com",
-             "shettyuttam@gmail.com", "esthappen90s@gmail.com",
+receivers = ["sangharshbyss@gmail.com", "punjajikhandare1974@gmail.com",
+             "kapilshivsharan37@gmail.com", "tribhuvanaseem@gmail.com",
+             "shettyuttam@gmail.com", "esthappen90s@gmail.com", "anupriyavidrohi@gmail.com",
              "akashsable08@gmail.com", "vickynandgaye.mils@gmail.com",
              "somubaba123@gmail.com", "amolnimsadkar@gmail.com", "mind.ravi@gmail.com",
              "dh.vivekmitra@gmail.com", "vipulk2703@gmail.com", "vinod.shinde124@gmail.com",
@@ -146,7 +162,7 @@ receivers = ["sangharshbyss@gmail.com", "kapilshivsharan37@gmail.com",
              "sukanya.shantha@gmail.com", "vishal.thakare09@gmail.com", "apeksha.jadhao1987@gmail.com",
              "tiss.pranay@gmail.com", "adv.kavitanw@gmail.com", "bysskishor@gmail.com",
              "vikramsonde@gmail.com", "amittikhade11@gmail.com", "adv.ambadasbansode@gmail.com",
-             "sharad.shelke@gmail.com", "aseemtribhuvan@gmail.com", "a1prashant@yahoo.com",
+             "sharad.shelke@gmail.com", "a1prashant@yahoo.com",
              "vaishupiyu14feb@gmail.com", "Shantikamal2217@gmail.com", "sachinkamble30@gmail.com",
              "ravindracw@gmail.com", "mind.ravi@gmail.com", "usabhi.milind@gmail.com",
              "sgedam40@yahoo.com", "disha.kad@gmail.com", "maitreyanath@gmail.com",
@@ -161,16 +177,19 @@ receivers = ["sangharshbyss@gmail.com", "kapilshivsharan37@gmail.com",
              "mekalesantosh@gmail.com", "sbtt.1119@gmail.com", "rrupesh88@gmail.com",
              "sneha.magar1992@gmail.com", "vanitatumsare@gmail.com", "uttam.madane@gmail.com",
              "sreejithmanu@gmail.com", "ambhore.766266@gmail.com", "aniltharayath@gmail.com",
-             "sorenlakhindar@gmail.com", "sawantsuresh@gmail.com", "thinker.atul@gmail.com"]
+             "sorenlakhindar@gmail.com", "sawantsuresh@gmail.com", "sumedhsgondane@gmail.com",
+             "vivekbansode81@gmail.com", "shreyaskamble95@gmail.com", "preranadhende3@gmail.com",
+             "yogothegreat@gmail.com", "vijay_sonkamble@yahoo.com"]
 # sending the email
 for name in receivers:
     yag.send(to=name,
              subject='(Marathi)Update: Complaints registered '
-                     'under PoA on 18-19 January 2021',
+                     'under PoA on 28-29 January 2021',
              contents=the_text,
-             attachments=['/home/sangharsh/Documents/PoA/data/FIR/January/copies/20012021 _ 21012021/rptFIR_Publish_Citizen.pdf',
-                          '/home/sangharsh/Documents/PoA/data/FIR/January/copies/18012021 _ 19012021/rptFIR_Publish_Citizen(1).pdf',
-                          '/home/sangharsh/Documents/PoA/data/FIR/January/copies/18012021 _ 19012021/rptFIR_Publish_Citizen(2).pdf',
-                          '/home/sangharsh/Documents/PoA/data/FIR/January/copies/18012021 _ 19012021/rptFIR_Publish_Citizen(3).pdf'])
+             attachments=['/home/sangharsh/Documents/PoA/data/FIR/January/copies/28012021 _ 29012021/rptFIR_Publish_Citizen(3).pdf',
+                          '/home/sangharsh/Documents/PoA/data/FIR/January/copies/28012021 _ 29012021/rptFIR_Publish_Citizen(4).pdf',
+                          '/home/sangharsh/Documents/PoA/data/FIR/January/copies/28012021 _ 29012021/rptFIR_Publish_Citizen(5).pdf',
+                          '/home/sangharsh/Documents/PoA/data/FIR/January/copies/28012021 _ 29012021/rptFIR_Publish_Citizen(6).pdf',
+                          '/home/sangharsh/Documents/PoA/data/FIR/January/copies/28012021 _ 29012021/rptFIR_Publish_Citizen(7).pdf'])
 
-print(f'Email succesfully sent to {len(receivers) } participants')
+print(f'Email successfully sent to {len(receivers) } participants')
